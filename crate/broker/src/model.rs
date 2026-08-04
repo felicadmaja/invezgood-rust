2148fa0a030c2dc6107afbda411383c325de9706
Felik Admaja <felic.admaja@gmail.com>
2026-08-04 13:36:34 +0700

    chore: auto-commit after 3 prompts (2026-08-04 06:36:34 UTC)

--- crate/broker/src/model.rs
+++ crate/broker/src/model.rs
@@ -1,10 +1,13 @@
-//! Model baris tabel `invezgood.broker`.
+//! Model baris tabel `invezgood.broker` + `invezgood.broker_stalker`.
+
+use std::collections::HashMap;
 
 use scylla::DeserializeRow;
 use scylla::SerializeRow;
 
 pub const KEYSPACE: &str = "invezgood";
 pub const TABLE: &str = "broker";
+pub const TABLE_BROKER_STALKER: &str = "broker_stalker";
 
 /// Satu baris `invezgood.broker`.
 #[derive(Debug, Clone, DeserializeRow, SerializeRow)]
@@ -20,4 +23,16 @@ pub struct BrokerRow {
     pub catatan: Option<String>,
     #[scylla(default_when_null)]
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+/// Satu baris `invezgood.broker_stalker`.
+/// PK: ((broker_code), tahun_bulan DESC).
+#[derive(Debug, Clone, DeserializeRow, SerializeRow)]
+pub struct BrokerStalkerRow {
+    pub broker_code: String,
+    pub tahun_bulan: String,
+    #[scylla(default_when_null)]
+    pub summary: Option<HashMap<String, String>>,
+    #[scylla(default_when_null)]
+    pub list: Option<Vec<HashMap<String, String>>>,
 }
--- todolist.md
+++ todolist.md
@@ -1,3 +1,8 @@
 curl 'https://api.invezgo.com/analysis/order-book/SIDO?market=RG&date=2026-08-04&time=10%3A25' \
 
 untuk menampilkkan order book, ditampikan di chart terpisah.
+
+
+
+mengintip aktivitas broker
+curl 'https://api.invezgo.com/analysis/stalker/list/AK?from=2026-06-01&to=2026-08-04&investor=all&market=RG' \
